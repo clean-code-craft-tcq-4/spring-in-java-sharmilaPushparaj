1b33734e393c8518b968263edee7e767027c5f3f
sharmilaPushparaj <sharmila.pushparaj2@gmail.com>
2022-05-23 20:31:06 +0530

    Create StatsChecker.java

---
+++ src/main/java/statisticker/StatsChecker.java
@@ -0,0 +1,26 @@
+package statisticker;
+import java.util.List;
+
+public class StatsChecker{
+        EmailAlert emailAlerter = new EmailAlert();
+        LEDAlert ledAlerter = new LEDAlert();
+        IAlerter alerters[] = null;
+        float maxThreshold =0f;
+        
+        public StatsChecker(float maxThreshold, IAlerter[] alerters){
+          this.maxThreshold =maxThreshold;
+          this.alerters=alerters;
+        }
+        
+        public void checkAndAlert(List<float> numbers){
+           float max=numbers.get(0);
+           for(int i=0;i<numbers.size();i++){
+                if(max<numbers.get(i))
+                        max=numbers.get(i);
+           }
+           if(max>maxThreshold){
+                 alerters[0].emailSent=true;
+                 alerters[1].ledGlows=true;
+           }
+        }
+}
